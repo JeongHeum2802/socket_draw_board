bc173fb68c08a5f8c39d09f3a66a0d99fa98cab4
최정흠 <wjdgma60@gmail.com>
2025-11-11 19:15:12 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const express = require("express");
+
+const { Server } = require("socket.io");
+const { clearInterval } = require("timers");
+
+const app = express();
+
+// public 폴더 정적 서빙
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(PORT, () => {
+    console.log("5001 포트에서 실행 중");
+});
+
+const io = new Server(server, {
+    path: "/socket.io",
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("클라이언트 접속 : ", socket.id);
+    clearInterval(socket.interval);
+
+    socket.on("disconnect", () => {
+        console.log("연결 끊김");
+    });
+
+    // 그림 좌표를 받으면 모든 socket에 리턴
+    socket.on("paint", (data) => {
+        io.emit("paintAll", data);
+    });
+
+    // 펜을 up했을 때
+    socket.on("upPen", () => {
+        io.emit("upPenAll");
+    });
+
+    // 펜을 다운했을 때
+    socket.on("downPen", () => {
+        io.emit("downPenAll");
+    });
+});
+
---
+++ public/index.js
@@ -0,0 +1,74 @@
+// socket io
+const socket = io("http://localhost:5001", {
+    path: "/socket.io",
+    transports: ["websocket"],
+});
+
+// 캔버스 설정
+const canvas = document.getElementById("canvasJs");
+var ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+let painting = false;
+
+// 그리기 취소
+function stopPainting() {
+    socket.emit("upPen");
+}
+
+// 그리기 시작
+function startPainting() {
+    socket.emit("downPen");
+}
+
+// 마우스 움직일 때 (캔버스 위에서)
+function onMouseMove(event) {
+
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if (painting) {
+        socket.emit("paint", { x, y });
+    }
+}
+
+// 지우기 버튼을 눌렀을 때
+function onClickEraseButton() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+// 캔버스 이벤트 리스너
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+// 지우기 버튼 이벤트 리스너
+const eraseBtn = document.getElementById("eraseBtn");
+if (eraseBtn) {
+    eraseBtn.addEventListener("click", onClickEraseButton)
+}
+
+// 다른 클라에서 서버로 보낸 x, y좌표를 서버에서 받음
+socket.on("paintAll", (data) => {
+    ctx.lineTo(data.x, data.y);
+    ctx.stroke();
+})
+
+// 펜 다운 업을 socket에서 주는 데이터로 컨트롤
+socket.on("upPenAll", () => {
+    painting = false;
+    console.log("펜 업");
+})
+
+socket.on("downPenAll", () => {
+    painting = true;
+    console.log("펜 다운");
+    ctx.beginPath();
+})
+
